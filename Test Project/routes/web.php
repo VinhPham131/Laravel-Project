2933fd135d4bf42849c697879024aedeeb9bc898
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-08-15 11:27:23 +0700

    API

--- Test Project/routes/web.php
+++ Test Project/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', \App\Http\Controllers\API\CategoryController::class);
+    Route::apiResource('products', \App\Http\Controllers\API\ProductController::class);
+});
+
