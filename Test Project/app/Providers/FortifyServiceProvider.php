d4c2610bc981d501453119b31e2e8cb686b7cbef
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-08-22 00:17:57 +0700

    Profile Management

--- Test Project/app/Providers/FortifyServiceProvider.php
+++ Test Project/app/Providers/FortifyServiceProvider.php
@@ -58,6 +58,11 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.reset-password', ['request' => $request]);
         });
 
+        //Authorize Email
+        Fortify::verifyEmailView(function () {
+            return view('auth.verify-email');
+        });
+
 
     }
 }
