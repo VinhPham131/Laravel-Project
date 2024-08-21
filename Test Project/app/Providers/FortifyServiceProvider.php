f243c5513dce971f7a4e3979f92b2fabfa6225ee
VinhPham131 <vinh.pham220212@vnuk.edu.vn>
2024-08-22 00:27:25 +0700

    Build API Token Management

--- Test Project/app/Providers/FortifyServiceProvider.php
+++ Test Project/app/Providers/FortifyServiceProvider.php
@@ -63,6 +63,15 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.verify-email');
         });
 
+        //Two Factor Authentication
+        Fortify::confirmPasswordView(function () {
+            return view('auth.confirm-password');
+        });
+
+        Fortify::twoFactorChallengeView(function () {
+            return view('auth.two-factor-challenge');
+        });
+
 
     }
 }
